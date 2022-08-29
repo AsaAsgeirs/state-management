4842a34d0d4d6a20d82c06c23c2cd6920ec5c0ec
Ása Þórdís Ásgeirsdóttir <asathordisasgeirsdottir@Asas-MacBook-Pro.local>
2022-08-29 13:13:27 +0000

    styles

---
+++ pages/ecommerce/ecommerce-context-selector.js
@@ -0,0 +1,59 @@
+import {
+    useLogin,
+    useLogout,
+    useAddToCart,
+    useUser,
+    useCartCount,
+    StoreContextProvider,
+} from "./store-context-selector";
+
+const LoginSection = () => {
+    const login = useLogin();
+    const logout = useLogout();
+    return (
+    <div>
+        <button onClick={login}>Login</button>
+        <button onClick={logout}>Logout</button>
+    </div>
+    )
+}
+
+const UserSection = () => {
+    const user = useUser();
+    return <div>User: {user}</div>;
+  };
+
+const AddToCartSection = () => {
+    const addToCart = useAddToCart();
+    return (
+    <div>
+        <button onClick={addToCart}>Add To Cart</button>
+    </div>
+    );
+}
+
+const CartCounterSection =() => {
+    const cartCount = useCartCount();
+    return (
+    <div>Cart count: {cartCount} </div>
+    );
+}
+
+function ContextPage() {
+    return (
+        <div>
+            <LoginSection />
+            <UserSection />
+            <AddToCartSection />
+            <CartCounterSection />
+        </div>
+    );
+}
+
+export default function ContextPageWrapper() {
+    return (
+        <StoreContextProvider>
+            <ContextPage />
+        </StoreContextProvider>
+    );
+}
